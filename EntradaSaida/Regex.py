cce16c41cf2f0f217bc2b27a2a08a34bafc55dde
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-09-19 20:22:21 -0300

    inicio

---
+++ EntradaSaida/Regex.py
@@ -0,0 +1,9 @@
+class Regex :
+
+    def newRegex(self, linhas):
+        for i in linhas:
+            traduzir(linhai)
+
+
+    def traduzir(self, linha):
+        pattern = "^\s*(\w+)\s+(\w+)\s*,\s*(\w+)\s*$"
