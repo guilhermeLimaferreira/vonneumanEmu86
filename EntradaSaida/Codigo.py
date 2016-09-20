957f6bc57a9305bb63cc606c64314b66866a9ac1
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-09-20 01:05:07 -0300

    'regex'

--- EntradaSaida/Codigo.py
+++ EntradaSaida/Codigo.py
@@ -1,1 +1,7 @@
-class Codigo():
+class Codigo:
+
+    def __init__(self, args):
+        self.parametros = []
+
+        for i in args:
+            self.parametros.append(i)
--- EntradaSaida/Regex.py
+++ EntradaSaida/Regex.py
@@ -1,9 +1,30 @@
-class Regex :
-
-    def newRegex(self, linhas):
-        for i in linhas:
-            traduzir(linhai)
+import re
+import Codigo
 
 
-    def traduzir(self, linha):
-        pattern = "^\s*(\w+)\s+(\w+)\s*,\s*(\w+)\s*$"
+class Regex:
+
+    def __init__(self, linhas):
+        self.linhas = linhas
+        self.traducao = []
+        self.traduzir()
+
+    def traduzir(self):
+        pattern = r"^\s*(\w+)\s+(\w+)\s*,\s*(\w+)\s*$" # pattern está incorreto para a multiplicacao
+        patternmult = r"^\s*(\w+)\s+(\w+)\s*,\s*(\w+)\s*,\s*(\w+)\s*$" # só funciona multiplicacao
+
+        comp = re.compile(pattern)
+        comp2 = re.compile(patternmult)
+
+        for i in self.linhas:
+            m = None
+
+            m = comp.findall(i)
+            if len(m) == 0:
+                m = comp2.findall(i) # aqui deve ser colocado o pattern da multiplicação
+
+            codigo = Codigo(m)
+
+            self.traducao.append(codigo)
+
+        return self.traducao
